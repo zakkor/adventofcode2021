48a73bdb0d548c9cde66803d8703f25eff6ddf64
zakkor <edward.partenie@gmail.com>
2021-12-07 12:01:23 +0200

    day 6: add immutable solution

--- day6_part2.ml
+++ day6_part2.ml
@@ -8,6 +8,29 @@ let initial_fish =
   List.hd input_lines |> String.split_on_char ',' |> List.map int_of_string
 
 let simulate days =
+  let count_day day = Base.List.count initial_fish ~f:(fun f -> f = day) in
+  let counts =
+    ( count_day 0,
+      count_day 1,
+      count_day 2,
+      count_day 3,
+      count_day 4,
+      count_day 5,
+      0,
+      0,
+      0 )
+  in
+  let rec step acc counts =
+    let d0, d1, d2, d3, d4, d5, d6, d7, d8 = counts in
+    if acc = 0 then counts
+    else step (acc - 1) (d1, d2, d3, d4, d5, d6, d7 + d0, d8, d0)
+  in
+  let d0, d1, d2, d3, d4, d5, d6, d7, d8 = step days counts in
+  d0 + d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8
+
+let () = printf "immutable: %d\n" @@ simulate 256
+
+let simulate_mut days =
   (* 0 to 8 days *)
   let counts = Array.make 9 0 in
   List.iter (fun f -> counts.(f) <- counts.(f) + 1) initial_fish;
@@ -25,4 +48,4 @@ let simulate days =
   done;
   Array.fold_left ( + ) 0 counts
 
-let () = printf "%d\n" @@ simulate 256
+let () = printf "mutable: %d\n" @@ simulate_mut 256
